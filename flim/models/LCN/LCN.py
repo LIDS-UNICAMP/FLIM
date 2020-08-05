e197806cf5a4b28accef0e337f60045e2d42afca
Italos Estilon <italosestilon@gmail.com>
2020-08-05 13:11:08 -0300

    add docstrings to LCN

--- flim/models/LCN/LCN.py
+++ flim/models/LCN/LCN.py
@@ -4,31 +4,66 @@ import torch.nn as nn
 import logging
 
 class LIDSConvNet(nn.Sequential):
+    """A convolutional neural network.
+
+    For now, it has only a feature extracor and no classifier.
+
+    Attributes
+    ----------
+    feature_extractor : nn.Torch.Module
+        A feature extractor formed by convolutional layers.
+    """  
     def __init__(self):
         super(LIDSConvNet, self).__init__()
         
         self.feature_extractor = nn.Sequential()
 
-        self.logger = logging.getLogger()
+        self._logger = logging.getLogger()
         
-    def forward(self, X, return_layeerwise_output=False):
-        self.logger.info("doing forward")
+    def forward(self, X):
+        """Apply the network to an input tensor.
 
-        if return_layeerwise_output:
-            output_by_layer = {}
+        Apply the network to a batch of images.
+
+        Parameters
+        ----------
+        x : torch.Tensor
+            Input tensor in the shape :math:`(N, C, H, W)`.
+
+        Returns
+        -------
+        torch.Tensor
+            Output tensor with shape :math:`(N, C^\prime,  H, W)`. 
+            The output height and width depends on the paramenters used to define the layer.
+        """        
+        self._logger.info("doing forward")
+
         for layer_name, layer in self.feature_extractor.named_children():
             y = layer.forward(X)
             X = y
-
-            if return_layeerwise_output:
-                output_by_layer[layer_name] = y.detach().cpu()
-
-        if return_layeerwise_output:
-            return y, output_by_layer
         
         return y
 
     def to(self, device):
+        """Move layer to ``device``.
+
+        Move layer parameters to some specified device.
+
+        Parameters
+        ----------
+        device : torch.device
+            The device where to move.
+
+        Returns
+        -------
+        Self
+            The layer itself.
+
+
+        Notes
+        ---------
+        This method modifies the module in-place.
+        """          
         for _, layer in self.feature_extractor.named_children():
            layer.to(device)
         
