72c5b39b9ec0e2ef2a6110398524ce17141ba450
Italos Estilon <italosestilon@gmail.com>
2020-08-06 21:01:34 -0300

    update package setup

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='FLIM',
     version='0.1',
@@ -7,11 +10,26 @@ setup(
     author_email='italosestilon@gmail.com',
     packages=setuptools.find_packages(),
     description='A package to build deep learning models from images markers.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/italosestilon/FLIM",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 2 - Pre-Alpha",
+        "Environment :: Console :: GPU :: NVIDIA CUDA",
+        "Intended Audience :: Education :: Developers",
+        "Natural Language :: English",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence :: Image Recognition",
+        "Topic :: Software Development :: Libraries"
+    ],
     install_requires=[
         "torch",
         "numpy",
         "scikit-image",
         "sklearn",
         "setuptools"
-    ]
+    ],
+    python_requires='>=3.6'
 )
