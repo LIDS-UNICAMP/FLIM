29587d2af8cc995baab3d2c7442142a61144209e
Italos Estilon <italosestilon@gmail.com>
2020-08-07 13:05:57 -0300

    setup linters

---
+++ flim/models/lcn/_lcn.py
@@ -0,0 +1,79 @@
+# noqa: D100
+
+import logging
+
+import torch.nn as nn
+
+__all__ = ["LIDSConvNet"]
+
+
+class LIDSConvNet(nn.Sequential):
+
+    """A convolutional neural network.
+
+    For now, it has only a feature extracor and no classifier.
+
+    Attributes
+    ----------
+    feature_extractor : nn.Torch.Module
+        A feature extractor formed by convolutional layers.
+
+    """
+
+    def __init__(self):
+        """Initialize the class."""
+        super(LIDSConvNet, self).__init__()
+        self.feature_extractor = nn.Sequential()
+        self._logger = logging.getLogger()
+
+    def forward(self, x):
+        """Apply the network to an input tensor.
+
+        Apply the network to a batch of images.
+
+        Parameters
+        ----------
+        x : torch.Tensor
+            Input tensor in the shape :math:`(N, C, H, W)`.
+
+        Returns
+        -------
+        torch.Tensor
+            Output tensor with shape :math:`(N, C^\prime,  H, W)`. 
+            The output height and width depends on the paramenters \
+            used to define the layer.
+
+        """
+        self._logger.info("doing forward")
+
+        for _, layer in self.feature_extractor.named_children():
+            _y = layer.forward(x)
+            x = _y
+
+        return _y
+
+    def to(self, device):
+        """Move layer to ``device``.
+
+        Move layer parameters to some specified device.
+
+        Parameters
+        ----------
+        device : torch.device
+            The device where to move.
+
+        Returns
+        -------
+        Self
+            The layer itself.
+
+
+        Notes
+        -----
+        This method modifies the module in-place.
+
+        """
+        for _, layer in self.feature_extractor.named_children():
+            layer.to(device)
+
+        return self
