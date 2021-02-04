5805eedecb9dcb7a42c39294dc466737ed0ccc60
Italos Estilon de Souza <italosestilon@gmail.com>
2021-02-04 17:08:29 -0300

    fix relabel function

--- flim/utils.py
+++ flim/utils.py
@@ -38,13 +38,13 @@ def label_connected_components(label_images, start_label=1):
 
     new_label_images = np.zeros_like(label_images).astype(np.int)
 
+    _c = start_label
+
     for label_image, new_label_image in zip(label_images, new_label_images):
         num_labels = label_image.astype(np.int32).max()
 
         #new_label_image = np.zeros_like(label_image).astype(np.int)
         structure = np.ones((3, 3), dtype=np.uint8)
-
-        _c = start_label
 
         for _l in range(1, num_labels + 1):
             _label_image = label(label_image == _l, structure=structure)[0]
