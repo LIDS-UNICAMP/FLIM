e673cacfe37e2fb6348cd102b5652bf66f378458
Italos Estilon <italosestilon@gmail.com>
2021-07-07 11:26:40 -0300

    remove GradientAscent

--- flim/experiments/utils.py
+++ flim/experiments/utils.py
@@ -46,8 +46,6 @@ from skimage.color import lab2rgb
 from ..models.lcn import LCNCreator, SpecialConvLayer, SpecialLinearLayer, LIDSConvNet
 from ._dataset import LIDSDataset
 
-from ._gradient_ascent import GradientAscent
-
 from PIL import Image
 
 import re
